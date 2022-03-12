4b156954bd81ef19dce6dbb5fca4164f5fa58131
ValeriTodorov123 <92388865+ValeriTodorov123@users.noreply.github.com>
2022-03-12 21:43:01 +0200

    Update start_page.dart

--- lib/start_page.dart
+++ lib/start_page.dart
@@ -9,20 +9,17 @@ class DomPage extends StatefulWidget {
 
 class _DomPageState extends State<DomPage> {
   @override
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container( 
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/pepe.jpg"),
-                fit: BoxFit.cover,
-            ),
-          ),
-
+      backgroundColor: Colors.black26,
+      body: InkWell(
           child: Stack(
             children: <Widget>[ 
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[ new Padding(padding: 
+<<<<<<< Updated upstream
                     const EdgeInsets.symmetric(horizontal: 200, vertical: 100)),
                     new SizedBox(
                       width: 200.0,
@@ -51,6 +48,31 @@ class _DomPageState extends State<DomPage> {
                             },
                           ),
                       )
+=======
+                    const EdgeInsets.symmetric(horizontal: 200, vertical: 200)),
+                      Text(
+                        
+                      ),
+                      ElevatedButton(
+                        child: const Text('Explore Planets!'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PlanetPage()),
+                        );
+                        },
+                      ),
+                      const SizedBox(height: 30),
+                    ElevatedButton(
+                        child: const Text('See NASA News!'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => NasaNewsPage()),
+                        );
+                        },
+                      ),
+>>>>>>> Stashed changes
                     ]
                 )
             ]
