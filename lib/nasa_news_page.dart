342f2cdf088bd6d04fe4920a27cda3897f9a9514
tomyslav <tomyivanov06@gmail.com>
2022-03-12 19:36:14 +0200

    bobi

--- lib/nasa_news_page.dart
+++ lib/nasa_news_page.dart
@@ -1,4 +1,4 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:newproject/start_page.dart';
 //import 'package:flutter/dart.core';
 
@@ -8,7 +8,6 @@ class NasaNewsPage extends StatefulWidget {
 }
 
 class _NasaNewsPageState extends State<NasaNewsPage> {
-  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,4 +46,4 @@ class Article
   }
 
 
-}*/
+}
--- lib/start_page.dart
+++ lib/start_page.dart
@@ -12,35 +12,34 @@ class _DomPageState extends State<DomPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('First Route'),
-      ),
       body: InkWell(
           child: Stack(
-            children: <Widget>[
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
+            children: <Widget>[ 
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[ new Padding(padding: 
+                    const EdgeInsets.symmetric(horizontal: 200, vertical: 0)),
+                      ElevatedButton(
+                        
+                        child: const Text('Explore Planets!'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => PlanetPage()),
+                        );
+                        },
+                      ),
                     ElevatedButton(
-                      child: const Text('Explore Planets!'),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => PlanetPage()),
-                       );
-                      },
-                    ),
-                   /* TextButton(
-                      child: const Text('See NASA News!'),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => NasaNewsPage()),
-                       );
-                      },
-                    ),*/
-                  ]
-              )
+                        child: const Text('See NASA News!'),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => NasaNewsPage()),
+                        );
+                        },
+                      ),
+                    ]
+                )
             ]
           )
       ),
