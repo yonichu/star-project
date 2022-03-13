3aa58e965025ad5ed5effef16ef772ac8bf56d1c
yonichuu <yoanna.stamenova@gmail.com>
2022-03-13 04:14:24 +0200

    Remove ignored files

--- lib/api.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:convert';
-import 'package:http/http.dart';
-import 'package:model/model.dart';
-
-class HttpService {
-  final String dirURL = "https://api.nasa.gov/planetary/apod";}
-  //ok
-  //mvlare
--- lib/constants.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-Color primaryTextColor = Color.fromARGB(255, 255, 255, 255);
-Color secondaryTextColor = Color(0xFFE4979E);
-Color titleTextColor = Colors.white;
-Color contentTextColor = Color.fromARGB(255, 255, 255, 255);
-Color gradientStartColor = Color(0xFF0050AC);
-Color gradientEndColor = Color(0xFF9354B9);
